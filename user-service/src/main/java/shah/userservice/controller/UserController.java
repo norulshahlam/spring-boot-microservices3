faf16663562b673f17085490574106d182fb2543
Norulshahlam <norulshahlam@gmail.com>
2021-11-19 00:57:04 +0800

    g

--- user-service/src/main/java/shah/userservice/controller/UserController.java
+++ user-service/src/main/java/shah/userservice/controller/UserController.java
@@ -2,21 +2,31 @@ package shah.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("/user")
 public class UserController {
 
 	@Value("${app.description}")
+	private String description;
 	
-    private String description;
 	@Autowired
 	private Environment env;
-
+	
+	
+	@EventListener({RefreshScopeRefreshedEvent.class})
+	public void onEvent() {
+		System.out.println("NEW VALUE:"+description);
+	}
+	
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working from user-service on port: "+env.getProperty("local.server.port")+" and instance id: "+env.getProperty("eureka.instance.instance-id");
@@ -24,5 +34,5 @@ public class UserController {
 	@GetMapping("/status/check-property")
 	public String checkProperty() {
 		return description;
-		}
+	}
 }
