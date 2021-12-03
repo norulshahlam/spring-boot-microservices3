bd6a1369f7026e1fe3f7608211bbce38023067d5
norulshahlam <norulshahlam@gmail.com>
2021-12-03 08:08:52 +0800

    r

---
+++ user-service/src/test/java/shah/userservice/controller/UserControllerTest.java
@@ -0,0 +1,39 @@
+package shah.userservice.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	@Test
+	void testOnEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckProperty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUserController() {
+		fail("Not yet implemented");
+	}
+
+}
